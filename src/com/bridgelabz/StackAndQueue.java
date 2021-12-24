4de231d24c941c88400e48c618f731dc424b3fa2
uma-kamuni <kamuniumaramesh@gmail.com>
2021-12-23 18:25:00 -0800

    Added: Ability To Create Stack

--- src/com/bridgelabz/StackAndQueue.java
+++ src/com/bridgelabz/StackAndQueue.java
@@ -1,9 +1,43 @@
 package com.bridgelabz;
 
 public class StackAndQueue {
+	Node top;
+
+	public void push(int x) {
+		Node newNode = new Node(x);
+		if (isEmpty()) {
+			top = newNode;
+		} else {
+			newNode.data = x;
+			newNode.next = top;
+			top = newNode;
+		}
+	}
+
+	public boolean isEmpty() {
+		return top == null;
+	}
+
+	public void display() {
+		if (top == null) {
+			System.out.printf("\nStack Underflow");
+		} else {
+			Node temp = top;
+			while (temp != null) {
+				System.out.print(temp.data + " ");
+				temp = temp.next;
+			}
+		}
+	}
 
 	public static void main(String[] args) {
-		System.out.println("Welcome To Stack And Queue Program");
+		System.out.println("Welcome Stack and Queue program");
+		StackAndQueue list = new StackAndQueue();
+		list.push(70);
+		list.push(30);
+		list.push(56);
+		System.out.println("The Stack elements are ");
+		list.display();
 	}
 
 }
